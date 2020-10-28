83afb3bf3e02354e7cb0982d72f3a3476f0dfc4f
Mariana Baroni <59779525+maribaroni@users.noreply.github.com>
2020-10-28 01:13:06 -0400

    Create CyclicRotation.js

---
+++ CyclicRotation.js
@@ -0,0 +1,30 @@
+/*
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+*/
+
+function solution(A, K) {
+
+    let i = 0;
+    
+    var B=[];
+    while (i < K)
+    {
+        var temp = [];
+        for (let j = 0; j < A.length; j++)
+        {
+            if (j == A.length-1)
+            {
+                temp[0] = A[j];
+            }
+            else 
+            {
+                temp[j+1] = A[j];
+            }
+        }
+        i++;
+        A=temp;
+    }
+    return temp;
+}
